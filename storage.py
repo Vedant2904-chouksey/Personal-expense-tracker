378331ba0b303a05adb91d211c84c5894798fb24
Vedant2904-chouksey <vedantstudyemail1@gmail.com>
2025-11-24 22:52:44 +0530

    Add files via upload

---
+++ storage.py
@@ -0,0 +1,16 @@
+expenses = []
+
+def Addexpense(date, category, amount, note):
+    expense = {
+        "date": date,
+        "category": category,
+        "amount": amount,
+        "note": note
+    }
+    expenses.append(expense)
+
+def getexpenses():
+    return expenses
+
+def clearexpenses():
+    expenses.clear()
